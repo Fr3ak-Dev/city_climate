d219572a01b4e61d0fa83d55e380644fbd3a4ec3
gustavo <kg.2k.pl@gmail.com>
2025-05-06 21:15:44 -0400

    Refactor Form component to manage state for city and country selection

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css';
+import { SearchType } from "../../types";
 
 export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <form className={styles.form}>
             <div className={styles.field}>
@@ -11,11 +26,19 @@ export default function Form() {
                     type="text"
                     name="city"
                     placeholder="City"
+                    value={search.city}
+                    onChange={handleChange}
                 />
             </div>
             <div className={styles.field}>
                 <label htmlFor="country">Country:</label>
-                <select>
+                <select
+                    id="country"
+                    name="country"
+                    value={search.country}
+                    onChange={handleChange}
+                    // className="text-black"
+                >
                     <option value="">-- Select a country --</option>
                     {countries.map(country => (
                         <option key={country.code} value={country.code}>
