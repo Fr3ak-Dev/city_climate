9e74cd3ebd4425029ab181b289944e46cda1ae25
gustavo <kg.2k.pl@gmail.com>
2025-05-07 20:58:49 -0400

    call to an API with Axios

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,7 +1,17 @@
+import axios from 'axios'
+import { SearchType } from '../types';
+
 export default function useWeather() {
 
-    const fetchWeather = () => {
-        console.log('Fetching weather data...');
+    const fetchWeather = async (search: SearchType) => {
+        const apiKey = '4d1c3e0ab7de35719e59bd89284356ab'
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`
+            const { data } = await axios(geoUrl);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return {
