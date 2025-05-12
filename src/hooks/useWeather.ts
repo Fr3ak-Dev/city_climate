00279c54a530daedab60d521e33f11cbafd7531a
gustavo <kg.2k.pl@gmail.com>
2025-05-11 23:04:38 -0400

    Refactor fetchWeather function to get weather data

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -8,7 +8,14 @@ export default function useWeather() {
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`
             const { data } = await axios(geoUrl);
-            console.log(data);
+
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+            const { data: weatherData } = await axios(weatherUrl);
+            console.log(weatherData);
         } catch (error) {
             console.log(error);
         }
