92eeb2259f8bc99094243782f0c2cf3be1428730
gustavo <kg.2k.pl@gmail.com>
2025-05-14 20:48:52 -0400

    Add type guard for weather response validation in useWeather hook

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,19 @@
 import axios from 'axios'
 import { SearchType, Weather } from '../types';
 
+// Type Guard or Assertions
+// Type Guards are functions that return a boolean value indicating whether the provided value is of a specific type.
+function isWeatherResponse(weather: unknown) : weather is Weather {
+    return (
+        Boolean(weather) &&
+        typeof weather === 'object' &&
+        typeof (weather as Weather).name === 'string' &&
+        typeof (weather as Weather).main.temp === 'number' &&
+        typeof (weather as Weather).main.temp_min === 'number' &&
+        typeof (weather as Weather).main.temp_max === 'number'
+    )
+}
+
 export default function useWeather() {
 
     const fetchWeather = async (search: SearchType) => {
@@ -14,9 +27,21 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
-            const { data: weatherData } = await axios<Weather>(weatherUrl)
-            console.log(weatherData.name)
-            console.log(weatherData.main.temp)
+            // Cast the type
+            // const { data: weatherResult } = await axios<Weather>(weatherUrl)
+            // console.log(weatherResult.name)
+            // console.log(weatherResult.main.temp)
+
+            // Type Guards
+            const { data: weatherResult } = await axios<Weather>(weatherUrl)
+            const result = isWeatherResponse(weatherResult)
+            if (result) {
+                console.log(weatherResult.name)
+                console.log(weatherResult.main.temp)
+            } else {
+                console.log('Invalid weather data')
+            }
+            
         } catch (error) {
             console.log(error);
         }
