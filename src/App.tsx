d1e6f2c9cc5d8c6210ba0ac018b7b33d3a43d020
gustavo <kg.2k.pl@gmail.com>
2025-05-15 19:44:07 -0400

    Add weather detail component and integrate weather state in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
 import Form from './components/Form/Form'
+import WeatherDetail from './components/WeatherDetail/WeatherDetail';
 import useWeather from './hooks/useWeather';
 
 function App() {
 
-  const { fetchWeather } = useWeather();
+  const { weather, fetchWeather } = useWeather();
 
   return (
     <>
@@ -12,6 +13,9 @@ function App() {
       <div className={styles.container}>
         <Form
           fetchWeather={fetchWeather}
+        />
+        <WeatherDetail 
+        weather={weather}
         />
       </div>
     </>
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,44 +1,29 @@
 import axios from 'axios'
-// import { z } from 'zod'
-import { object, string, number, InferOutput, parse } from 'valibot'
+import { z } from 'zod'
 import { SearchType } from '../types';
-
-// Type Guard or Assertions
-// Type Guards are functions that return a boolean value indicating whether the provided value is of a specific type.
-// function isWeatherResponse(weather: unknown) : weather is Weather {
-//     return (
-//         Boolean(weather) &&
-//         typeof weather === 'object' &&
-//         typeof (weather as Weather).name === 'string' &&
-//         typeof (weather as Weather).main.temp === 'number' &&
-//         typeof (weather as Weather).main.temp_min === 'number' &&
-//         typeof (weather as Weather).main.temp_max === 'number'
-//     )
-// }
+import { useState } from 'react';
 
 // Zod Schema
-// const Weather = z.object({
-//     name: z.string(),
-//     main: z.object({
-//         temp: z.number(),
-//         temp_min: z.number(),
-//         temp_max: z.number()
-//     })
-// })
-// type Weather = z.infer<typeof Weather>
-
-// Valibot Schema
-const WeatherSchema = object({
-    name: string(),
-    main: object({
-        temp: number(),
-        temp_min: number(),
-        temp_max: number()
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_min: z.number(),
+        temp_max: z.number()
     })
 })
-type Weather = InferOutput<typeof WeatherSchema>
+export type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
+
+    const [weather, setWeather] = useState<Weather>({
+        name: '',
+        main: {
+            temp: 0,
+            temp_min: 0,
+            temp_max: 0
+        }
+    })
 
     const fetchWeather = async (search: SearchType) => {
         const apiKey = import.meta.env.VITE_API_KEY;
@@ -51,36 +36,11 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
-            // Cast the type
-            // const { data: weatherResult } = await axios<Weather>(weatherUrl)
-            // console.log(weatherResult.name)
-            // console.log(weatherResult.main.temp)
-
-            // Type Guards
-            // const { data: weatherResult } = await axios<Weather>(weatherUrl)
-            // const result = isWeatherResponse(weatherResult)
-            // if (result) {
-            //     console.log(weatherResult.name)
-            //     console.log(weatherResult.main.temp)
-            // } else {
-            //     console.log('Invalid weather data')
-            // }
-
             // Zod
-            // const { data: weatherResult } = await axios(weatherUrl)
-            // const result = Weather.safeParse(weatherResult)
-            // if (result.success) {
-            //     console.log(result.data.name)
-            //     console.log(result.data.main.temp)
-            // } else {
-            //     console.log('Invalid weather data')
-            // }
-
-            // Valibot
             const { data: weatherResult } = await axios(weatherUrl)
-            const result = parse(WeatherSchema, weatherResult)
-            if (result) {
-                console.log(result)
+            const result = Weather.safeParse(weatherResult)
+            if (result.success) {
+                setWeather(result.data)
             }
 
         } catch (error) {
@@ -89,6 +49,7 @@ export default function useWeather() {
     }
 
     return {
+        weather,
         fetchWeather
     }
 }
